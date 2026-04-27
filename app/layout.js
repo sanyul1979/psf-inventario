b8e120f36047ec515debf37a9f37bbdf3119dfc5
sanyul1979 <RAULAND98@HOTMAIL.COM>
2026-04-27 17:54:13 -0600

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,13 @@
+export const metadata = {
+    title: 'PSF Inventario',
+    description: 'Dashboard de Productos Terminados',
+};
+
+export default function RootLayout({ children }) {
+    return (
+          <html lang="es">
+            <head />
+            <body>{children}</body>
+      </html>
+    );
+}
